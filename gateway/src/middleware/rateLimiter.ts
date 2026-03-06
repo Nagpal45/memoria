8cf6da0bf15169ad1e7dfb4efc217c5c18ae9f32
Vaibhav Nagpal <vaibhavnagpal.dev@gmail.com>
2026-03-06 08:48:34 +0530

    refactor(rateLimiter): clean up code by commenting out unused fixed window implementation

--- gateway/src/middleware/rateLimiter.ts
+++ gateway/src/middleware/rateLimiter.ts
@@ -46,3 +46,30 @@ export const rateLimiter = async (
     next();
   }
 };
+
+
+//fixed window 
+
+// export const rateLimiter = async (req: Request, res: Response, next: NextFunction) => {
+//     const ip = req.ip || req.socket.remoteAddress || 'unknown';
+
+//     const limit = 10;
+//     const windowInSeconds = 60;
+
+//     try{
+//         const requests = await redisClient.incr(ip);
+
+//         if (requests === 1) {
+//             await redisClient.expire(ip, windowInSeconds);
+//         }
+
+//         if (requests > limit) {
+//             res.status(429).json({ message: 'Too many requests. Please try again in a minute.', status: 429 });
+//             return;
+//         }
+//         next();
+//     }catch (err) {
+//         console.error('Error in rate limiter:', err);
+//         next();
+//     }
+// };
